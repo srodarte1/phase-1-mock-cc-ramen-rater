722154c1997e2de9a3d4ceea116de179b136bc65
+ <rodarte_sarah@yahoo.com>
2022-12-01 14:34:31 -0700

    Finish code challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// write your code here
+const handleClick = (e) => {
+    //we click on ramenImg, and are able to see all the details of the ramen displayed inside ramen-detail 
+    e.target.ramenImg = ramenObj.value
+
+}
+document.addEventListener('click', handleClick)
+
+
+const displayRamens = (ramenObj) => {
+    const ramenDiv = document.getElementById('ramen-menu')
+    const ramenImg = document.createElement('img')
+    ramenImg.src = ramenObj.image
+    ramenDiv.appendChild(ramenImg)
+    const ramenDetails = document.getElementById('ramen-detail')
+    const ramenName = document.getElementsByClassName('name')
+    const ramenRest = document.getElementsByClassName('restaurant')
+    const ramenRating = document.getElementById('rating-display')
+    
+    
+}
+
+// const ramenObj = {}
+const fetchRamens = () => {
+    fetch("http://localhost:3000/ramens")
+   .then(resp => resp.json())
+   .then(ramenObj => ramenObj.forEach(displayRamens))
+}
+fetchRamens()
+
